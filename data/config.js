cfc1951693f30642c111599bfc196ae9bcfee078
Cut0x <71967731+Cut0x@users.noreply.github.com>
2023-04-19 00:44:56 +0200

    Update config.js

--- data/config.js
+++ data/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    token: "ODI1MDY3NDI2NjI2MTQyMjg5.G2_a5b.9OJyR2YWHRw9KDl7pH-E3ko58GiNKuvSrKdgjY",
-    clientId: "825067426626142289"
+    token: "token",
+    clientId: "id_bot"
 };
