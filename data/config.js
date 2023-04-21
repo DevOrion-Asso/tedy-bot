d91ed3588a60e9c55e8c5ca84b53cde4e5d3abbe
Cut0x <71967731+Cut0x@users.noreply.github.com>
2023-04-21 18:20:36 +0200

    Update config.js

--- data/config.js
+++ data/config.js
@@ -1,4 +1,12 @@
 module.exports = {
     token: "token",
-    clientId: "id_bot"
+    clientId: "id_bot",
+
+    guildId: "id_serveur_support",
+    clientId: "id_client",
+
+    BotClose: false, // put true if you want only you can do the orders
+
+    emojiYes: "emoji_oui",
+    emojiNo: "emoji_non"
 };
