0598b139eb6ab99b6df31829d9ff30f8ef1ef35b
Cut0x <71967731+Cut0x@users.noreply.github.com>
2023-04-21 18:21:34 +0200

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,13 +1,52 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, Events, EmbedBuilder, ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
 const wait = require('node:timers/promises').setTimeout;
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('test')
-		.setDescription('test'),
-	async execute(interaction, client, db) {
-        await interaction.reply({
-            content: `test`
-        });
+		.setDescription('Faire des test !')
+        .addStringOption(option =>
+            option.setName('select-event')
+                .setDescription('Choisissez l\'évènement à lancer !')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'guildMemberAdd', value: 'guildmemberadd' },
+                    { name: 'guildMemberRemove', value: 'guildmemberremove' },
+                )),
+	async execute(client, interaction) {
+		const event = interaction.options.getString("select-event");
+
+		if (interaction.user.id !== "id_owner_bot") return interaction.reply({
+			embeds: [
+				new EmbedBuilder()
+					.setColor('Red')
+					.setDescription(`${client.config.emojiNo} Vous devez être le développeur du bot pour faire cette commande !`)
+			],
+			ephemeral: true
+		})
+
+		if (event == "guildmemberadd") {
+			client.emit(Events.GuildMemberAdd, interaction.member);
+
+			interaction.reply({
+				embeds: [
+					new EmbedBuilder()
+						.setColor('Green')
+						.setDescription(`${client.config.emojiYes} Lancement de \`${event}\` réussi !`)
+				],
+				ephemeral: true
+			});
+		} else if (event == "guildmemberremove") {
+			client.emit(Events.GuildMemberRemove, interaction.member);
+
+			interaction.reply({
+				embeds: [
+					new EmbedBuilder()
+						.setColor('Green')
+						.setDescription(`${client.config.emojiYes} Lancement de \`${event}\` réussi !`)
+				],
+				ephemeral: true
+			});
+		};
 	},
 };
