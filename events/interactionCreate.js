33640bd7f1b14ef44565b5cd62817e3029aa0269
Cut0x <71967731+Cut0x@users.noreply.github.com>
2023-04-21 18:22:41 +0200

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -3,8 +3,7 @@ const wait = require('node:timers/promises').setTimeout;
 
 const colors = require("colors/safe");
 
-const { QuickDB } = require('quick.db');
-const db = new QuickDB();
+const { BotClose } = require("../data/config");
 
 const client = require("../main");
 
@@ -12,33 +11,67 @@ module.exports = {
 	name: Events.InteractionCreate,
 	once: false,
 	async execute(interaction) {
-        if (!interaction.isChatInputCommand()) return;
-
-        const command = interaction.client.commands.get(interaction.commandName);
+        if (interaction.isChatInputCommand()) {
+            const command = interaction.client.commands.get(interaction.commandName);
+        
+            if (!command) {
+                console.error(`No order match ${interaction.commandName} was found.`);
+                return;
+            }
+        
+            try {
+                if (BotClose == true && interaction.user.id !== "id_owner_bot") {
+                    await interaction.deferReply({ ephemeral: true });
+                    await wait(2000);
     
-        if (!command) {
-            console.error(`No order match ${interaction.commandName} was found.`);
-            return;
-        }
+                    await interaction.editReply({
+                        embeds: [
+                            new EmbedBuilder()
+                                .setColor('Red')
+                                .setDescription(':x: Bot blocked, only developer can use the bot !')
+                        ],
+                        ephemeral: true
+                    })
+                } else {
+                    await command.execute(client, interaction);
+                };
+            } catch (error) {
+                console.log(colors.magenta('[ERROR]') + "\n" + colors.red(error));
     
-        try {
-            if (db.get(`bot_close`) == true && !interaction.members.roles.has('1098013995107110983')) {
-                await interaction.deferReply({ ephemeral: true });
-                await wait(2000);
-
                 await interaction.editReply({
                     embeds: [
                         new EmbedBuilder()
                             .setColor('Red')
-                            .setDescription(':x: Bot en maintenance !')
+                            .setDescription(`:x: An error occurred while running the command \`${interaction.commandName}\` !`)
+                            .addFields([
+                                {
+                                    name: "> Error :",
+                                    value: "```diff\n- " + error + "```"
+                                }
+                            ])
                     ],
                     ephemeral: true
-                })
-            } else {
-                await command.execute(interaction, client, db);
+                });
+            }
+        };
+
+        if (interaction.isModalSubmit()) {
+            const modal = client.modals.get(interaction.customId);
+        
+            if (!modal) return interaction.reply({
+              embeds: [
+                new EmbedBuilder()
+                  .setDescription('Something went wrong... Probably the Modal ID is not defined in the modals handler.')
+                  .setColor('Red')
+              ],
+              ephemeral: true
+            });
+        
+            try {
+              modal.run(client, interaction);
+            } catch (e) {
+              console.error(e)
             };
-        } catch (error) {
-            //console.log(colors.magenta('[ERROR]') + "\n" + colors.red(error));
         }
 	},
 };
