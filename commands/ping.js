ff59780e8b8d99c51767d37fa3bfddec81ef9c7e
Cut0x <71967731+Cut0x@users.noreply.github.com>
2023-04-21 18:21:13 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -5,13 +5,14 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Envoie la latence du bot !'),
-	async execute(interaction, client, db) {
-        const mesg = await interaction.deferReply({ content: ":ping_pong: Pong !", fetchReply: true });
+	async execute(client, interaction) {
+        const mesg = await interaction.deferReply({ content: ":ping_pong: Pong !", fetchReply: true, ephemeral: true });
 		
         await wait(3000);
 		
         await interaction.editReply({
-            content: `:ping_pong: Pong !\n> Here is my latency: \`${mesg.createdTimestamp - interaction.createdTimestamp} ms\``
+            content: `:ping_pong: Pong !\n> Voici ma latence : \`${mesg.createdTimestamp - interaction.createdTimestamp} ms\``,
+            ephemeral: true
         });
 	},
 };
