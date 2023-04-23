13c6f4966e644db29f7b8f089dfea12dd355acc7
Cut0x <71967731+Cut0x@users.noreply.github.com>
2023-04-23 23:06:24 +0200

    Add files via upload

---
+++ data/emojis.js
@@ -0,0 +1,14 @@
+module.exports = {
+    yes: "<:check:1099376357856784516>",
+    no: "<:cross:1099376359383519292>",
+    arrow: "<:arrow:1099378513942954107>",
+    shield: "<:shield:1099376362164322425>",
+    annonce: "<:announcements:1099376355143069756>",
+    github: "<:github:1099377158729773066>",
+    twitter: "<:twitter:1099377165465817178>",
+    insta: "<:insta:1099377161154068560>",
+    paypal: "<:paypal:1099377162630467716>",
+    purplecercle: "<:purple_cercle:1099378807380648036>",
+    tedyWink: "<:TedyWink:998366976881737748>",
+    wave: "<:wave:1099411674110382100>"
+};
