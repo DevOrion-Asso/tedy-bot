2bd5003ef935b72b2b59f726a3f9eafc3c72bdf6
Cut0x <71967731+Cut0x@users.noreply.github.com>
2023-04-25 00:11:23 +0200

    Update config.js

--- data/config.js
+++ data/config.js
@@ -2,5 +2,10 @@ module.exports = {
     token: "token",
     clientId: "id_bot",
 
-    guildId: "id_serveur_support"
+    ownerBot: "574544938440851466",
+
+    guildId: "id_serveur_support",
+    suggestChannel: "id_salon_suggestion",
+    categorieTicket: "id_catégorie_ticket",
+    supportTicketRole: "id_rôle_support"
 };
