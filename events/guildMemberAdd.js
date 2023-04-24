2421cd407c459e1494e7707446068f49c1123517
Cut0x <71967731+Cut0x@users.noreply.github.com>
2023-04-25 00:09:46 +0200

    Add files via upload

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -8,15 +8,15 @@ module.exports = {
 	name: Events.GuildMemberAdd,
 	once: false,
 	async execute(member) {
-        const channel = member.guild.channels.cache.find(channel => channel.id === 'id_salon_bienvenue');
+        const channel = member.guild.channels.cache.find(channel => channel.id === '998374421800689735');
 
-        member.roles.add('id_role_membre').catch(error => {});
+        member.roles.add('998366139946115162').catch(error => {});
 
         if (channel) channel.send({
             embeds: [
                 new EmbedBuilder()
                     .setColor('Blurple')
-                    .setDescription(`<:arrow:1097959138807521280> Bienvenue ${member} !\n> Vas regarder <#1098777615936012380> <:TedyWink:998366976881737748>`)
+                    .setDescription(`${client.emoji.wave} Bienvenue ${member} !\n> Vas regarder <#1098777615936012380> ${client.emoji.tedyWink}`)
                     .setFooter({ text: `Nous sommes désormais ${member.guild.memberCount} membres !` })
                     .setImage('https://cdn.discordapp.com/attachments/914271938359210045/1098785367945121893/banner.png')
             ]
--- events/ready.js
+++ events/ready.js
@@ -2,13 +2,64 @@ const { Events, ActivityType, ActionRowBuilder, ButtonBuilder, ButtonStyle, Embe
 
 const colors = require("colors/safe");
 
-const { QuickDB } = require("quick.db");
-const db = new QuickDB();
-
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	async execute(c) {
         console.log(colors.blue('[INFO]') + " " + colors.red(`Log In on ${c.user.username} !`));
+
+		c.user.setPresence({
+			activities: [
+				{
+					name: 'vous aider !'
+				}
+			]
+		});
+
+		// MESSAGE INFORMATION
+		//const channelInfo = c.guilds.cache.get('998363750862168184').channels.cache.find(channel => channel.id === '1098777615936012380');
+
+		const embedInfo = new EmbedBuilder()
+			.setColor('#b83dba')
+			.setTitle("Information sur __DevOrion__")
+			.setThumbnail('https://cdn.discordapp.com/attachments/914271938359210045/980928769588072478/LOGO-DEVORION-1.png')
+			.setDescription(`${c.emoji.purplecercle} **DevOrion** est en procédure pour devenir une **association** !`)
+			.addFields([
+				{
+					name: "Bienvenue chez DevOrion,",
+					value: `Un groupe de développeurs dévoués qui travaillent sur la création de petits projets informatiques, fournissant aussi une assistance pour les particuliers.
+${c.emoji.arrow} En tant qu'équipe de développeurs, nous sommes engagés à créer des projets innovants qui **répondent parfaitement à vos besoins.** Nous travaillons en étroite collaboration avec nos clients pour comprendre leurs besoins et créer une solution adaptée.
+${c.emoji.arrow}  DevOrion offre une **gamme complète de services informatiques**, allant de la création de petits projets personnalisés à l'assistance informatique complète pour les particulier.
+${c.emoji.arrow}  Notre objectif est l'excellence, et nous sommes fiers de notre **expertise technique** mise à votre service !`
+				},
+				{
+					name: "Maintenant, des __éventuelles questions__ :",
+					value: `**Q1: Est-ce que vos services sont gratuits ?**
+Effectivement, nos prestations sont entièrement **gratuites et bénévoles.** 
+					
+**Q2: Quels besoins pouvez-vous satisfaire ?**
+Nous sommes en mesure de répondre à une variété de besoins, par exemple :
+${c.emoji.purplecercle} Compléter une partie de votre bot Discord que vous jugez trop difficile...
+${c.emoji.purplecercle}  Développer un site internet répondant à vos envies.
+${c.emoji.purplecercle}  Fournir une assistance avec des logiciels ou des appareils informatiques : crashes, interrogations... 
+> Ce ne sont que des exemples, notre démarche est de s'adapter à nos cs, en leur proposant un service personnalisé.`
+				},
+				{
+					name: ":information_source: __**Important:**__",
+					value: `Nous recherchons des personnes motivées pour rejoindre notre équipe ! Si vous êtes intéressé, contactez moi !
+
+
+Merci de votre attention. Nous sommes impatients de travailler avec vous et de vous aider à tirer le meilleur parti de la technologie ${c.emoji.tedyWink}
+
+
+*Voici notre __direction artistique__ :*`
+				}
+			])
+			.setImage('https://cdn.discordapp.com/attachments/1098308533952458762/1099336283454701609/DA-DevOrion.png')
+		/*if (channelInfo) channelInfo.send({
+			embeds: [
+				embedInfo
+			]
+		});*/
     },
 };
