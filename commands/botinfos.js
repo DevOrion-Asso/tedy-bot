b101b7359ac6b17f0488e45ceae8c7ed9d258c8e
Cut0x <71967731+Cut0x@users.noreply.github.com>
2023-04-25 22:00:44 +0200

    Create botinfos.js

---
+++ commands/botinfos.js
@@ -0,0 +1,57 @@
+const { SlashCommandBuilder } = require('discord.js');
+const wait = require('node:timers/promises').setTimeout;
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('botinfos')
+		.setDescription('Affiche toutes les informations du bot'),
+	async execute(client, interaction, db) {
+        let totalSeconds = (client.uptime / 1000);
+        let days = Math.floor(totalSeconds / 86400);
+        totalSeconds %= 86400;
+        let hours = Math.floor(totalSeconds / 3600);
+        totalSeconds %= 3600;
+        let minutes = Math.floor(totalSeconds / 60);
+        let seconds = Math.floor(totalSeconds % 60);
+        
+        let uptime = `**${days}** jour(s), **${hours}** heure(s), **${minutes}** minute(s) et **${seconds}** seconde(s)`;
+
+        const développeur = client.users.cache.get('574544938440851466');
+
+        interaction.reply({
+            embeds: [
+                new EmbedBuilder()
+                    .setColor('#b83dba')
+                    .setTitle(`Informations sur le bot`)
+                    .setThumbnail(client.user.avatarURL({ dynamic: true, format: "webp" }))
+                    .addFields(
+                        [
+                            {
+                                name: "Client :",
+                                value: `> **${client.user.tag}**\n> ${client.user}\n> \`${client.user.id}\``,
+                                inline: true
+                            },
+                            {
+                                name: "Structure :",
+                                value: `> **Discord.js@${version}**\n> <:slash_command:1095694892111503481> Pris en charge`,
+                                inline: true
+                            },
+                            {
+                                name: "** **",
+                                value: `** **`,
+                            },
+                            {
+                                name: "Développeur :",
+                                value: `> **${développeur.tag}** / [**DevOrion Developer Team**](https://github.com/orgs/DevOrion-Korp/)`,
+                                inline: true
+                            },
+                            {
+                                name: "Uptime du bot :",
+                                value: `> ${uptime}`,
+                            }
+                        ]
+                    )
+            ]
+        });
+	},
+};
